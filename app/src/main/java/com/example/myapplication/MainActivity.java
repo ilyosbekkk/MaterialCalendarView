eef48dc2e0304e571377fc11e8c66eaabb1c4a0d
Ilyosbek Ibrokhimov <46449085+ilyosbekibrohimov@users.noreply.github.com>
2021-03-13 02:27:19 +0900

    some changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,39 +2,57 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
 
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
 
     MaterialCalendarView calendarView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calendarView = findViewById(R.id.calendarView);
+        ArrayList<CalendarDay> days = new ArrayList<>();
+        ArrayList<CalendarDay> days1 = new ArrayList<>();
+        ArrayList<CalendarDay> days2 = new ArrayList<>();
 
-        calendarView.setSelectionMode (MaterialCalendarView.SELECTION_MODE_NONE);
+        days.add(CalendarDay.from(2021,3,8));
+        days.add(CalendarDay.from(2021,3,10));
+        days.add(CalendarDay.from(2021,3,13));
+        days.add(CalendarDay.from(2021,3,15));
+        days.add(CalendarDay.from(2021,3,16));
+        days1.add(CalendarDay.from(2021,3,15));
+        days1.add(CalendarDay.from(2021,3,13));
+        days2.add(CalendarDay.from(2021,3,14));
+        days1.add(CalendarDay.from(2021,3,17));
+        days1.add(CalendarDay.from(2021,3,18));
+        days2.add(CalendarDay.from(2021,3,19));
+        days1.add(CalendarDay.from(2021,3,30));
+        days1.add(CalendarDay.from(2021,3,26));
+        days2.add(CalendarDay.from(2021,3,23));
 
 
+        calendarView.addDecorator(new Sample1(1, days, this));
+        calendarView.addDecorator(new Sample1(2,  days1,  this));
+        calendarView.addDecorator(new Sample1(3,  days2,  this));
 
-
-        calendarView.setDateSelected(CalendarDay.from(2021,3,8), true);
-        calendarView.setDateSelected(CalendarDay.from(2021,3,9), true);
-
-        calendarView.addDecorators();
 
 
         calendarView.setOnDateLongClickListener((widget, date) -> {
@@ -42,11 +60,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+}
+
+class Sample1 implements DayViewDecorator {
+
+    private int answer_num;
+    private final ArrayList<CalendarDay> dates;
+    private Context context;
+
+    public Sample1(int level, ArrayList<CalendarDay> dates, Context context) {
+        this.answer_num = level;
+        this.dates = dates;
+        this.context = context;
+    }
 
 
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
 
-
-
+    @Override
+    public void decorate(DayViewFacade view) {
+        if (answer_num == 1)
+            view.setBackgroundDrawable(Objects.requireNonNull(ContextCompat.getDrawable(context, R.drawable.sample3)));
+        else if (answer_num == 2)
+            view.setBackgroundDrawable(Objects.requireNonNull(ContextCompat.getDrawable(context, R.drawable.sample2)));
+        else
+            view.setBackgroundDrawable(Objects.requireNonNull(ContextCompat.getDrawable(context, R.drawable.sample1)));
 
     }
 }
